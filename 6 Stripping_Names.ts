fac0105837027f844609318582d992b5a770b6e1
Abdul Razzaq <160563401+Abdul-razzaq-Baswaid@users.noreply.github.com>
2024-03-25 01:52:24 +0500

    Delete 6 Stripping_Names.ts

--- 6 Stripping_Names.ts
+++
@@ -1,9 +0,0 @@
-
-
-const personName :string = "   \n\tShahid Afridi\t\n   ";
-
-console.log(personName);
-
-const without_Whitespace :string = personName.trim();
-
-console.log(without_Whitespace);
